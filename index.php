b78fad3e5dfb9777653a5eb7fe55b80bf5c33711
RedstartDesignsUK <41233671+Vertanzil@users.noreply.github.com>
2022-04-14 15:57:58 +0100

    start

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
-
+use sqlBuilder\sqlBuilder;
 
 echo "This is a test";
+
+
+$sqlBuilder = new sqlBuilder();
+
+
+$sqlBuilder
+    ->select("*")
+    ->where("this = < 22");
+
+$sqlBuilder->toString();
