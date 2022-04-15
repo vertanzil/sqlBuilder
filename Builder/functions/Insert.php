07cccc91279f5d4bea3b23af0f2aea51ddb99be7
RedstartDesignsUK <41233671+Vertanzil@users.noreply.github.com>
2022-04-15 22:26:31 +0100

    Fixed #2

--- Builder/functions/Insert.php
+++ Builder/functions/Insert.php
@@ -8,38 +8,23 @@ include_once ("./Builder/interfaces/QueryInterface.php");
 
 class Insert implements QueryInterface
 {
-    /**
-     * @var string
-     */
     private $table;
-
-    /**
-     * @var array<string>
-     */
     private $columns = [];
-
-    /**
-     * @var array<string>
-     */
     private $values = [];
-
-    public function __construct(string $table)
-    {
+    public function __construct(string $table){
         $this->table = $table;
     }
 
-    public function __toString(): string
-    {
+    public function __toString(): string{
         return 'INSERT INTO ' . $this->table
             . ' (' . implode(', ',$this->columns) . ') VALUES (' . implode(', ',$this->values) . ')';
     }
-
-    public function columns(string ...$columns): self
-    {
+    public function columns(string ...$columns): self{
         $this->columns = $columns;
-        foreach ($columns as $column) {
-            $this->values[] = ":$column";
-        }
+        return $this;
+    }
+    public function values(string ...$values): self{
+        $this->values = $values;
         return $this;
     }
 }
--- index.php
+++ index.php
@@ -4,10 +4,15 @@ include_once ("Builder/QueryBuilder.php");
 include_once("Builder/functions/Select.php");
 include_once("Builder/functions/Insert.php");
 $query = (new QueryBuilder())
-    ->insert('COALESCE (count(clue.id))')
+    ->insert('table_name')
     ->columns("1", "2", "3")
-
-
-;
+    ->values("4", "5", "6");
 echo '<br>';
 echo $query;
+
+$query2 = (new QueryBuilder())
+    ->insert('table_name')
+    ->columns("1", "2", "3")
+    ->values("4", "5", "6");
+echo '<br>';
+echo $query2;
